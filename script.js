52456da6ba54f1963102410021e39eb20f7cc760
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2022-02-07 17:20:37 +0200

    Add more DOM intergration

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let playerScore = 0;
 let computerScore = 0;
 
 
-
 function computerPlay() {
     const computerNum = Math.floor(Math.random() * 3) + 1;
     let computerSelection;
@@ -17,7 +16,11 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const roundResult = document.querySelector('#result')
+    const roundResult = document.querySelector('#result');
+    const playerScoreCounter = document.querySelector('#playerScoreCount');
+    const computerScoreCounter = document.querySelector('#computerScoreCount');
+    playerScoreCounter.innerText = `${playerScore}`;
+    computerScoreCounter.innerText = `${computerScore}`;
 
     function playerWins() {
         playerScore++;
@@ -53,17 +56,30 @@ function playRound(playerSelection, computerSelection) {
             return playerWins();
         }
     }
-    const playerScoreCounter = document.querySelector('#playerScoreCount');
-    const computerScoreCounter = document.querySelector('#computerScoreCount');
-    playerScoreCounter.innerText = `${playerScore}`;
-    computerScoreCounter.innerText = `${computerScore}`;
 }
+
+function makeSelection() {
+    const rockSelection = document.querySelector('#rock').addEventListener('click', () => {
+        playerSelection === "Rock"
+    });
+    const paperSelection = document.querySelector('#paper').addEventListener('click', () => {
+        playerSelection === "Paper"
+    });
+    const scissorsSelection = document.querySelector('#scissors').addEventListener('click', () => {
+        playerSelection === "Scissors"
+    });
+    return playerSelection;
+}
+
+// window.addEventListener('load', game);
 
 function game() {
     while (playerScore !== 5 && computerScore !== 5) {
-        const playerInput = prompt("Enter your choice").toLowerCase();
-        const playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1);
+        // const playerSelection = prompt("enter selection");
+        makeSelection();
         const computerSelection = computerPlay();
+        const playerSelectionText = document.querySelector('#playerSelection').innerText = `${playerSelection}`
+        const computerSelectionText = document.querySelector('#computerSelection').innerText = `${computerSelection}`
         playRound(playerSelection, computerSelection);
         console.log(`Your score: ${playerScore}`);
         console.log(`Computer score: ${computerScore}`);
