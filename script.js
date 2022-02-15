6d4a13130a764e80df4994aa1e541a0ba5c9ea70
Adnan Cezar Zanbouah <cezarzanbouah@gmail.com>
2022-02-15 22:05:05 +0200

    Add endGame, playRock, playPaper, playScissors functions, add event listeners to RPS buttons, finalize UI

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let playerScore = 0;
 let computerScore = 0;
 const roundResult = document.querySelector('#result');
 updateScore();
+const endResult = document.querySelector('#endResultText');
 
 function computerPlay() {
     const computerNum = Math.floor(Math.random() * 3) + 1;
@@ -16,6 +17,19 @@ function computerPlay() {
     return computerSelection;
 }
 
+function gameEnd() {
+    if (playerScore || computerScore === 5) {
+        if (playerScore === 5) {
+            endResult.innerText = 'Congratulations! You won! Press the new game button to start over.';
+            removeListeners();
+        } else if (computerScore === 5) {
+            endResult.innerText = 'You lost! Better luck next time! Press the new game button to start over.';
+            removeListeners();
+        }
+    }
+}
+
+
 function updateScore() {
     const playerScoreCounter = document.querySelector('#playerScoreCount');
     const computerScoreCounter = document.querySelector('#computerScoreCount');
@@ -24,8 +38,8 @@ function updateScore() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    const playerSelectionText = document.querySelector('#playerSelect').innerText = `${playerSelection}`
-    const computerSelectionText = document.querySelector('#computerSelect').innerText = `${computerSelection}`
+    playerSelectionText.innerText = `${playerSelection}`
+    computerSelectionText.innerText = `${computerSelection}`
 
     if (playerSelection === computerSelection) {
         roundResult.innerText = 'Draw!'
@@ -65,33 +79,83 @@ function playRound(playerSelection, computerSelection) {
         computerScore++;
         roundResult.innerText = `You lose! ${playerSelection} gets beat by ${computerSelection}.`
     }
-
+    gameEnd();
 }
 
-const rockSelection = document.querySelector('#rock').addEventListener('click', () => {
+function playRock() {
     const playerSelection = "Rock"
     const computerSelection = computerPlay();
     return playRound(playerSelection, computerSelection);
-});
-const paperSelection = document.querySelector('#paper').addEventListener('click', () => {
+}
+
+function playPaper() {
     const playerSelection = "Paper"
     const computerSelection = computerPlay();
     return playRound(playerSelection, computerSelection);
-});
-const scissorsSelection = document.querySelector('#scissors').addEventListener('click', () => {
+}
+
+function playScissors() {
     const playerSelection = "Scissors"
     const computerSelection = computerPlay();
     return playRound(playerSelection, computerSelection);
-});
+}
 
-const startNewGame = document.querySelector('#newGameBtn').addEventListener('click', () => {
-    newGame();
+function removeListeners() {
+    rockSelection.removeEventListener('click', playRock);
+    paperSelection.removeEventListener('click', playPaper);
+    scissorsSelection.removeEventListener('click', playScissors);
+}
+
+const rockSelection = document.querySelector('#rock');
+rockSelection.addEventListener('click', playRock);
+rockSelection.addEventListener('mouseover', () => {
+    rockSelection.classList.toggle('hover');
+})
+rockSelection.addEventListener('mouseout', () => {
+    rockSelection.classList.toggle('hover');
 })
 
+const paperSelection = document.querySelector('#paper')
+paperSelection.addEventListener('click', playPaper);
+paperSelection.addEventListener('mouseover', () => {
+    paperSelection.classList.toggle('hover');
+})
+paperSelection.addEventListener('mouseout', () => {
+    paperSelection.classList.toggle('hover');
+})
+
+const scissorsSelection = document.querySelector('#scissors')
+scissorsSelection.addEventListener('click', playScissors);
+scissorsSelection.addEventListener('mouseover', () => {
+    scissorsSelection.classList.toggle('hover');
+})
+scissorsSelection.addEventListener('mouseout', () => {
+    scissorsSelection.classList.toggle('hover');
+})
+
+const newGameButton = document.querySelector('.newGameBtn')
+newGameButton.addEventListener('click', () => {
+    newGame();
+})
+newGameButton.addEventListener('mouseover', () => {
+    newGameButton.classList.toggle('hover');
+})
+newGameButton.addEventListener('mouseout', () => {
+    newGameButton.classList.toggle('hover');
+})
+
+const computerSelectionText = document.querySelector('#computerSelect')
+const playerSelectionText = document.querySelector('#playerSelect')
 
 function newGame() {
     playerScore = 0;
     computerScore = 0;
+    playerSelectionText.innerText = "";
+    computerSelectionText.innerText = "";
     roundResult.innerText = "";
+    endResult.innerText = "";
+    rockSelection.addEventListener('click', playRock);
+    paperSelection.addEventListener('click', playPaper);
+    scissorsSelection.addEventListener('click', playScissors);
     updateScore();
 }
